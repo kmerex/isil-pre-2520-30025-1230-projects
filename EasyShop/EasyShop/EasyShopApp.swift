c45ceaeb2b45a55f651f794cce2c7778e1736654
kmerex@gmail.com <Kmerex@gmail.com>
2025-09-18 03:03:51 -0400

    chore: initial project setup

---
+++ EasyShop/EasyShop/EasyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyShopApp.swift
+//  EasyShop
+//
+//  Created by user278707 on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct EasyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
